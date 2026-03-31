613590d31d0fcd58582112ee5cd6e100bb47bb56
Danh Quan <143018247+quan-tap-code@users.noreply.github.com>
2026-03-31 17:21:15 +0700

    feat: create initial structure for AIMS package with Aims, Cart, and DigitalVideoDisc classes docs: edit README.md

--- AimsProject/src/AIMS/DigitalVideoDisc.java
+++ AimsProject/src/AIMS/DigitalVideoDisc.java
@@ -1,4 +1,4 @@
-package Testing;
+package AIMS;
 
 public class DigitalVideoDisc {
     private String title;
@@ -27,10 +27,34 @@ public class DigitalVideoDisc {
         return cost;
     }
 
-    public DigitalVideoDisc(String title, String category, String directory, int length, float cost) {
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+        this.category = "";
+        this.directory = "";
+        this.length = 0;
+        this.cost = 0.0f;
+    }
+
+    public DigitalVideoDisc(String category, String title, float cost) {
         this.title = title;
         this.category = category;
-        this.directory = directory;
+        this.directory = "";
+        this.length = 0;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String director, String category, String title, float cost) {
+        this.title = title;
+        this.category = category;
+        this.directory = director;
+        this.length = 0;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.directory = director;
         this.length = length;
         this.cost = cost;
     }
--- README.md
+++ README.md
@@ -2,28 +2,11 @@
 
 All projects are created using IntelliJ IDEA 
 
+## AIMS
+
+[Lab 2](AimsProject)
+
 ## Lab01
 
-[answer.txt](Lab01/answer.txt) 
+[Lab 1](Lab01)
 
-[2.2.1](Lab01/2.2.1/) - HelloWorld
-
-[2.2.2](Lab01/2.2.2/) - FirstDialog
-
-[2.2.3](Lab01/2.2.3/) - HelloNameDialog
-
-[2.2.4](Lab01/2.2.4/) - ShowTwoNumbers
-
-[2.2.5](Lab01/2.2.5/) - CalculateTwoNumbers
-
-[2.2.6](Lab01/2.2.6/) - Equation
-
-[6.1_6.2](Lab01/6.1_6.2/) - ChoosingOption & InputFromKeyboard
-
-[6.3](Lab01/6.3/) - nStars
-
-[6.4](Lab01/6.4/) - DayMonthYear
-
-[6.5](Lab01/6.5/) - SumAverageArray
-
-[6.6](Lab01/6.6/) - AddTwoMatrices
