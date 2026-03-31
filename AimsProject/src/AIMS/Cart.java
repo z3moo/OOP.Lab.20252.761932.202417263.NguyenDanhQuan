e1335ff611f2d6e9bac91c1afbf9b7df6df326d0
Danh Quan <143018247+quan-tap-code@users.noreply.github.com>
2026-03-31 17:20:55 +0700

    feat: create initial structure for AIMS package with Aims, Cart, and DigitalVideoDisc classes docs: edit README.md

---
+++ AimsProject/src/AIMS/Cart.java
@@ -0,0 +1,7 @@
+package Testing;
+
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private DigitalVideoDisc itemsOrdered[] =
+            new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+}
---
+++ AimsProject/src/AIMS/DigitalVideoDisc.java
@@ -0,0 +1,45 @@
+package Testing;
+
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String directory;
+    private int length;
+    private float cost;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirectory() {
+        return directory;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String directory, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.directory = directory;
+        this.length = length;
+        this.cost = cost;
+    }
+
+
+
+
+
+
+
+
+}
