3c365c724228a92850810e5bda050673f47d4283
Danh Quan <143018247+quan-tap-code@users.noreply.github.com>
2026-04-14 16:41:17 +0700

    feat: implement DigitalVideoDisc methods for parameter passing

--- Lab03/SourceCode/AimsProject/src/AIMS/DigitalVideoDisc.java
+++ Lab03/SourceCode/AimsProject/src/AIMS/DigitalVideoDisc.java
@@ -1,11 +1,32 @@
 package AIMS;
 
 public class DigitalVideoDisc {
+
     private String title;
     private String category;
     private String director;
     private int length;
     private float cost;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
 
     public String getTitle() {
         return title;
@@ -14,7 +35,6 @@ public class DigitalVideoDisc {
     public String getCategory() {
         return category;
     }
-
 
     public String getDirector() {
         return director;
---
+++ Lab03/SourceCode/AimsProject/src/TestPassingParameter.java
@@ -0,0 +1,28 @@
+import AIMS.DigitalVideoDisc;
+
+public class TestPassingParameter {
+    public static void main(String[] args) {
+        //TODO Auto-generated method stub
+        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+
+        swap(jungleDVD, cinderellaDVD);
+        System.out.println("Jungle DVD title: " + jungleDVD.getTitle());
+        System.out.println("Cinderella DVD title: " + cinderellaDVD.getTitle());
+
+        changeTitle(jungleDVD, cinderellaDVD.getTitle());
+        System.out.println("Jungle DVD title: " + jungleDVD.getTitle());
+    }
+
+    public static void swap(Object o1, Object o2) {
+        Object temp = o1;
+        o1 = o2;
+        o2 = temp;
+    }
+
+    public static void changeTitle(DigitalVideoDisc dvd, String title) {
+        String oldTitle = dvd.getTitle();
+        dvd.setTitle(title);
+        dvd = new DigitalVideoDisc(oldTitle);
+    }
+}
