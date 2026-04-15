e7f4adc1fb4bc8d0086c093b0231bf98a3936b64
Danh Quan <143018247+quan-tap-code@users.noreply.github.com>
2026-04-15 13:35:15 +0700

    feat: implement GarbageCreator and NoGarbage classes

--- OtherProjects/src/hust/soict/itep/garbage/ConcatenationInLoops.java
+++ OtherProjects/src/hust/soict/itep/garbage/ConcatenationInLoops.java
@@ -1,5 +1,4 @@
 package hust.soict.itep.garbage;
-
 import java.util.Random;
 
 public class ConcatenationInLoops {
@@ -7,18 +6,27 @@ public class ConcatenationInLoops {
         Random r = new Random(123);
         long start = System.currentTimeMillis();
         String s = "";
-        for (int i = 0; i < 65536; i++) {
+        for (int i = 0; i < 65536; ++i) {
             s += r.nextInt(2);
         }
-        System.out.println(System.currentTimeMillis() - start);
+        System.out.println("String: " + (System.currentTimeMillis() - start) + " ms");
+
+        r = new Random(123);
+        start = System.currentTimeMillis();
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < 65536; ++i) {
+            stringBuffer.append(r.nextInt(2));
+        }
+        s = stringBuffer.toString();
+        System.out.println("StringBuffer: " + (System.currentTimeMillis() - start) + " ms");
 
         r = new Random(123);
         start = System.currentTimeMillis();
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 65536; i++) {
+        for (int i = 0; i < 65536; ++i) {
             sb.append(r.nextInt(2));
         }
         s = sb.toString();
-        System.out.println(System.currentTimeMillis() - start);
+        System.out.println("StringBuilder: " + (System.currentTimeMillis() - start) + " ms");
     }
 }
