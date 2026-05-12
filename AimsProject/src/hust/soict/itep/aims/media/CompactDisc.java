849a467a84781966c048aeed9159b6985b3dfb63
Danh Quan <143018247+quan-tap-code@users.noreply.github.com>
2026-05-12 16:14:21 +0700

    feat: refactor CompactDisc and Track classes

--- AimsProject/src/hust/soict/itep/aims/media/CompactDisc.java
+++ AimsProject/src/hust/soict/itep/aims/media/CompactDisc.java
@@ -1,6 +1,41 @@
 package hust.soict.itep.aims.media;
 
+import java.util.ArrayList;
+
 public class CompactDisc extends Disc {
+    private String artist;
+    private ArrayList<Track> tracks = new ArrayList<Track>();
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void addTrack(Track track) {
+        if (tracks.contains(track)) {
+            System.out.println("The track is already in the CD");
+        } else {
+            tracks.add(track);
+            System.out.println("The track has been added");
+        }
+    }
+
+    public void removeTrack(Track track) {
+        if (tracks.contains(track)) {
+            tracks.remove(track);
+            System.out.println("The track has been removed");
+        } else {
+            System.out.println("The track is not in the CD");
+        }
+    }
+
+    public int getLength() {
+        int totalLength = 0;
+        for (Track track : tracks) {
+            totalLength += track.getLength();
+        }
+        return totalLength;
+    }
+
     public CompactDisc() {
         super();
     }
@@ -9,15 +44,27 @@ public class CompactDisc extends Disc {
         super(title);
     }
 
+    public CompactDisc(String title, String artist) {
+        super(title);
+        this.artist = artist;
+    }
+
     public CompactDisc(String title, String category, float cost) {
         super(title, category, cost);
     }
 
-    public CompactDisc(String title, String category, String director, float cost) {
-        super(title, category, director, cost);
+    public CompactDisc(String title, String category, String artist, float cost) {
+        super(title, category, cost);
+        this.artist = artist;
     }
 
-    public CompactDisc(String title, String category, String director, int length, float cost) {
+    public CompactDisc(String title, String category, String director, String artist, float cost) {
+        super(title, category, director, cost);
+        this.artist = artist;
+    }
+
+    public CompactDisc(String title, String category, String director, String artist, int length, float cost) {
         super(title, category, director, length, cost);
+        this.artist = artist;
     }
 }
