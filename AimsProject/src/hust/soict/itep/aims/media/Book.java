57c968e6b3b59135428f5d7389963cf8276c5a64
Danh Quan <143018247+quan-tap-code@users.noreply.github.com>
2026-05-12 15:34:58 +0700

    feat: enhance Book class

--- AimsProject/src/hust/soict/itep/aims/media/Book.java
+++ AimsProject/src/hust/soict/itep/aims/media/Book.java
@@ -1,4 +1,45 @@
 package hust.soict.itep.aims.media;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Book {
+    private int id;
+    private String title;
+    private String category;
+    private float cost;
+    private List<String> authors = new ArrayList<String>();
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void addAuthor(String authorName) {
+        if (!authors.contains(authorName)) {
+            authors.add(authorName);
+        }
+    }
+
+    public void removeAuthor(String authorName) {
+        if (authors.contains(authorName)) {
+            authors.remove(authorName);
+        }
+    }
+
+    public Book() {
+
+    }
+
 }
